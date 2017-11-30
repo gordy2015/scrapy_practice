92bfd812b85dc3303114c67dcddb6360b210653c
kiki <kiki@c.com>
2017-11-30 16:07:01 +0800

    从zh大牛爬取他的关注者和粉丝们的用户信息并存入json和mongodb

--- zhihu/zhihu/pipelines.py
+++ zhihu/zhihu/pipelines.py
@@ -4,7 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import os,json
+import os
+import json
+import logging
 from zhihu import settings
 import pymongo
 
@@ -46,15 +48,21 @@ class member_mongodb(object):
         self.client.close()
 
     def process_item(self, item, spider):
-        # 这里通过mongodb进行了一个去重的操作，每次更新插入数据之前都会进行查询，判断要插入的url_token是否已经存在，如果不存在再进行数据插入，否则放弃数据
+        # 去重方法1) 这里通过mongodb进行了一个去重的操作，每次更新插入数据之前都会进行查询，判断要插入的url_token是否已经存在，如果不存在再进行数据插入，否则放弃数据
         self.db['member_detail'].update({'url_token': item["url_token"]}, {'$set': item}, True)
         # print(type(item))  #<class 'zhihu.items.ZhihuItem'>
 
 
-        # if self.db.users.find({"url_token": item['url_token']}):
-        #     raise DropItem("Duplicate item found: %s" % item)
+        # 去重方法2) 通过查询，并在插入前查询是否已存在
+        # di = {"url_token": item['url_token']} #url_token是唯一的,可用于查询 或直接用dict(item)查询
+        # w = self.db.member_detail.find_one(di)
+        # # print(type(w),bool(w),w)  #find_one如果查询到有结果会返回True, dict类型, 如到结果，返回False, 会是None类型
+        # if w:
+        #     logging.info("Duplicate item found: %s"%item['url_token'])
         # else:
-        #     self.db['user5'].insert_one(dict(item)) #这也是向db中插入数量，但没有对数据做去重操作
+        #     self.db['member_detail'].insert_one(dict(item))  # 这也是向db中插入数量，但没有对数据做去重操作
+
+
         return item
 
 
--- zhihu/zhihu/spiders/zhihu.py
+++ zhihu/zhihu/spiders/zhihu.py
@@ -9,22 +9,25 @@ class zihuSpider(scrapy.Spider):
     allowed_domains = ["zhihu.com"]
 
     start_urls = ['http://www.zhihu.com']
-    start_user = 'canfire'
-    member_urls = "https://www.zhihu.com/api/v4/members/{urltoken}?include=allow_message%2Cis_followed%2Cis_following%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics"
+    start_user = 'excited-vczh'
+    #start_user主动关注了谁
+    followees_url = "https://www.zhihu.com/api/v4/members/{user}/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20"
+    #start_user的粉丝（被关注）
+    follower_url = "https://www.zhihu.com/api/v4/members/{user}/followers?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=0&limit=20"
+    #用户详细信息
     member_detail = "https://www.zhihu.com/api/v4/members/{urltoken}?include=locations%2Cemployments%2Cgender%2Ceducations%2Cbusiness%2Cvoteup_count%2Cthanked_Count%2Cfollower_count%2Cfollowing_count%2Ccover_url%2Cfollowing_topic_count%2Cfollowing_question_count%2Cfollowing_favlists_count%2Cfollowing_columns_count%2Cavatar_hue%2Canswer_count%2Carticles_count%2Cpins_count%2Cquestion_count%2Ccolumns_count%2Ccommercial_question_count%2Cfavorite_count%2Cfavorited_count%2Clogs_count%2Cincluded_answers_count%2Cincluded_articles_count%2Cincluded_text%2Cmessage_thread_token%2Caccount_status%2Cis_active%2Cis_bind_phone%2Cis_force_renamed%2Cis_bind_sina%2Cis_privacy_protected%2Csina_weibo_url%2Csina_weibo_name%2Cshow_sina_weibo%2Cis_blocking%2Cis_blocked%2Cis_following%2Cis_followed%2Cis_org_createpin_white_user%2Cmutual_followees_count%2Cvote_to_count%2Cvote_from_count%2Cthank_to_count%2Cthank_from_count%2Cthanked_count%2Cdescription%2Chosted_live_count%2Cparticipated_live_count%2Callow_message%2Cindustry_category%2Corg_name%2Corg_homepage%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics"
 
-
-
     def start_requests(self):
-        # url = "https://www.zhihu.com/api/v4/members/feagle0311/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=20&limit=20'"
-        url = "https://www.zhihu.com/api/v4/members/{user}/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=20&limit=20"
-        yield scrapy.Request(url.format(user=self.start_user),callback=self.parse_urltoken)
+        yield scrapy.Request(self.followees_url.format(user=self.start_user),callback=self.parse_urltoken)
+        yield scrapy.Request(self.follower_url.format(user=self.start_user),callback=self.parse_urltoken)
+
 
     def parse_urltoken(self, response):
         # print(type(response.text),response.text)
         w = response.text
         p = json.loads(w)
         # print(type(p),p)
+        pagi = p['paging']
         for k,v in p.items():
             # print('%s:%s' %(k,v))
             if k == "data":
@@ -32,31 +35,23 @@ class zihuSpider(scrapy.Spider):
                 nu = 1
                 for i in v:
                     # print(type(i),i)
-                    s = 1
+                    # s = 1
                     for k,v in i.items():
                         # print('[{nu}-{s}] {k}:{v}'.format(nu=nu,k=k,v=v,s=s))
                         if k == 'url_token':
                             # print('[{nu}-{s}] {k}:{v}'.format(nu=nu,k=k,v=v,s=s))
                             yield scrapy.Request(self.member_detail.format(urltoken=v),callback=self.parse_member_url)
-                        s += 1
-                    nu += 1
+                        # s += 1
+                    # nu += 1
+
+        if not pagi['is_end']:
+                # print('next_page: %s'%pagi['next'])
+                yield scrapy.Request(pagi['next'],callback=self.parse_urltoken)
 
     def parse_member_url(self,response):
         item = ZhihuItem()
-
         m = json.loads(response.text)
-        # print(item)
-        # print(m)
-
-        # for k,v in m.items():
-        #     # print("%s:%s" %(k,v))
-        #     item[k] = v
-        #     yield item
-
-
         for fields in item.fields:
             if fields in m.keys():
                 item[fields] = m.get(fields)
-        #         # print('[{mn}] {k}:{v}'.format(k=k,v=v,mn=mn))
-
         yield item
